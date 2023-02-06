6d53c4b3e62b11b715151b9f08c1c8e60714b627
ixchelmn <64459748+ixchelmn@users.noreply.github.com>
2023-02-06 02:12:54 -0600

    Implementacion de Spinner

--- app/src/main/java/com/example/pm011p2023/ActivityPrincipal.java
+++ app/src/main/java/com/example/pm011p2023/ActivityPrincipal.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class ActivityPrincipal extends AppCompatActivity
 {
     Button btn_ingresar;
-    Button btn_lista;
+    Button btn_lista, btn_combo;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,8 +19,10 @@ public class ActivityPrincipal extends AppCompatActivity
 
         btn_ingresar = (Button) findViewById(R.id.btn_ingresar);
         btn_lista = (Button) findViewById(R.id.btn_lista);
+        btn_combo = (Button) findViewById(R.id.btn_combo);
 
-        btn_ingresar.setOnClickListener(new View.OnClickListener() {
+        btn_ingresar.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view)
             {
@@ -29,11 +31,22 @@ public class ActivityPrincipal extends AppCompatActivity
             }
         });
 
-        btn_lista.setOnClickListener(new View.OnClickListener() {
+        btn_lista.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view)
             {
                 Intent intent = new Intent(getApplicationContext(), ActivityListView.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_combo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent  intent = new Intent(getApplicationContext(),ActivityCombo.class);
                 startActivity(intent);
             }
         });
